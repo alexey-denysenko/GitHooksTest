115338e809b2a08f9f05b5872b2a9af5f5ca74ca
Oleksii Denysenko <oleksii_denysenko@epam.com>
2018-01-31 16:15:07 +0200

    Initial commit

--- app/src/main/java/com/example/olden/githookstest/MainActivity.java
+++ app/src/main/java/com/example/olden/githookstest/MainActivity.java
@@ -12,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
     }
